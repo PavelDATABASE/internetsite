f2efa25b0bdec3984467ad00c67a55c181182c03
kpk304kab <kpk304kab@outlook.com>
2023-05-02 10:22:45 +0500

    add file project

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style1.css">
+	<link rel="icon" href="images/logo.png" type="image/icon type">
+	<title>Веб-программирование</title>
+</head>
+<body>
+	<div class="header">
+		<div class="container">
+			<div class="header__inner">
+				<div class="logo">
+					<a href="index.html">
+						<img src="images/logo.png" alt="logo">
+					</a>
+			
+				</div>
+				<div class="button__list">
+					<div class="button__item">
+						<a href="index.html" class="button__link">Главная</a>
+					</div>
+					<div class="button__item">
+						<a href="index3.html" class="button__link">UX-разработчик</a>
+					</div>
+					<div class="button__item">
+						<a href="index2.html" class="button__link">Польза</a>
+					</div>
+					<div class="button__item">
+						<a href="auth.html" class="button__link">Auth/Reg</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="content">
+		<!-- <div class="container__left">
+			<h1>Отзывы</h1>
+			<div class="sectionesag"></div>
+		<div class="container__object">
+			<div class="container__object-mini">
+				<img src="http://zornet.ru/_fr/83/7890600.jpg" alt="">
+				<div class="object__name">Андрей</div>
+				<div class="object__star">
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+				</div>
+				<p>Отличный сайт!</p>
+			</div>
+		</div>
+				<div class="container__object">
+			<div class="container__object-mini">
+				<img src="http://zornet.ru/_fr/83/2047084.jpg" alt="">
+				<div class="object__name">Дмитрий</div>
+				<div class="object__star">
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="far fa-star"></i>
+					<i class="far fa-star"></i>
+				</div>
+				<p>Немного не понял как пройти регистрацию</p>
+			</div>
+		</div> -->
+		</div>
+		<div class="container__right">
+		</div>	
+		<div class="container">
+			<div class="content__inner">
+				<div class="content__title">
+				Веб-программирование
+				</div>
+				<div class="content__image">
+					<img src="images/web.jpg" alt="">
+				</div>
+				<div class="content__title1">
+					Почему стоит идти в веб?
+				</div>
+				<div class="content__item">
+					<div class="content__item-title">
+						1. Веб-разработка - это интересно
+					</div>
+					<div class="content__item-text">
+						Стоит изучить одну технологию и начать с ней работать, как через два-три месяца на рынке появится что-то совершенно новое и все начнут использовать именно это. С вебом не соскучитесь.
+					</div>
+				</div>
+				<div class="content__item">
+					<div class="content__item-title">
+						2. Веб-разработка - это творчество
+					</div>
+					<div class="content__item-text">
+						Веб-разработка постоянно пополняется молодыми специалистами с новыми идеями, а сама сфера — новыми инструментами, возможностями и сервисами.
+					</div>
+				</div>
+				<div class="content__item">
+					<div class="content__item-title">
+						3. Веб-разработка - это развитие
+					</div>
+					<div class="content__item-text">
+						Senior-программистов с десятью годами стажа можно встретить довольно редко: либо из них вырастают управленцы, либо они учатся новому. Бурный рост профессии формирует широкое информационное поле и крепкое сообщество. Поэтому, если вы молоды, полны энергии, имеете чувство вкуса, да ещё и с любовью относитесь к программированию, веб-разработка — для вас.
+					</div>
+				</div>
+			</div>
+				<!-- <section class="grid">
+    	<article class="grid-item">
+        <div class="image">
+            <img src="images/ux.jpg" />
+        </div>
+        <div class="info">
+            <h2>Курс UX-дизайнера</h2>
+            <div class="info-text">
+                <p align="center">Очень полезный курс для начинающих веб-разработчиков</p>
+            </div>
+            <div class="button-wrap">
+                <a class="atuin-btn" href="https://netology.ru/programs/product-design-ultimate?utm_source=yandex&utm_medium=cpc&utm_campaign=bdes_diu_ou_ya_search&utm_content=9217887827&utm_term=%D0%BA%D1%83%D1%80%D1%81%D1%8B%20ux%20%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD%D0%B5%D1%80%D0%B0&yclid=6019737079807793353&stop=1#/">Подробнее</a>
+            </div>
+        </div>
+    	</article>
+    
+    	<article class="grid-item">
+        <div class="image">
+            <img src="images/ux2.jpg" />
+        </div>
+        <div class="info">.atuin-btn:after,
+			.atuin-btn:before {
+				position: absolute;
+				height: 4px;
+				left: 50%;
+				background: #2e4ef0;
+				bottom: -6px;
+				content: "";
+				transition: all 280ms ease-in-out;
+				width: 0;
+			}
+            <h2>Курс <br>маркетинга</h2>
+            <div class="info-text">
+                <p align="center">Маркетинг - важный навык для любого веб-программиста</p>
+            </div>
+            <div class="button-wrap">
+                <a class="atuin-btn" href="https://gb.ru/geek_university/internet-marketing?utm_source=yandex.ru&utm_medium=cpc&utm_campaign=yandex_s_rus_marketolog_lp_university_internet_marketing_marketing&utm_term=курсы%20маркетинга&utm_content=cid:58757628%7Cgid:4440648128%7Caid:10189820870%7Cpt:premium%7Cpos:3%7Cst:search%7Csrc:none%7Cdvc:desktop%7Creg:11207%7Cadph:no%7Capt:none%7Cret:%7Cdsa:25652741773&yclid=6027771313616640754">Подробнее</a>
+            </div>
+        </div>
+    	</article>
+    	<article class="grid-item">
+        <div class="image">
+            <img src="images/html.jpg" />
+        </div>
+        <div class="info">
+            <h2>Курс базовых знаний HTML</h2>
+            <div class="info-text">
+                <p align="center">Без этого языка верстки вы не сможете работать :)</p>
+            </div>
+            <div class="button-wrap">
+                <a class="atuin-btn" href="https://skillbox.ru/course/frontend-pro-expert/?utm_source=yandex&utm_medium=cpc&utm_campaign=841_frontend-pro-expert_yandex_cpc_poisk_course_all_all_gizatullin_60968216&utm_content=adg_4540254063%7Cad_10615760114%7Cph_31195105324%7Ckey_хтмл%20курс%7Cdev_desktop%7Cpst_premium_2%7Crgnid_11207_Копейск%7Cplacement_none%7Ccreative_%7Bcreative_name%7D&utm_term=хтмл%20курс&ad2020&yclid=6027778001805038831">Подробнее</a>
+            </div>
+        </div>
+    	</article>    
+    	<article class="grid-item">
+        <div class="image">
+            <img src="images/design.jpg" />
+        </div>
+        <div class="info">
+            <h2>Курс дизайн решений</h2>
+            <div class="info-text">
+                <p align="center">Данный курс позволит наладить зрительное понимание дизайна</p>
+            </div>
+            <div class="button-wrap">
+                <a class="atuin-btn" href="https://gb.ru/courses/all/design?utm_source=yandex.ru&utm_medium=cpc&utm_campaign=yandex_s_rus_design_gu_all_design&utm_term=курсы%20дизайна&utm_content=cid:63339244%7Cgid:4618526296%7Caid:10889495181%7Cpt:premium%7Cpos:3%7Cst:search%7Csrc:none%7Cdvc:desktop%7Creg:11207%7Cadph:no%7Capt:none%7Cret:%7Cdsa:32751833486&yclid=6027786172010389879">Подробнее</a>
+            </div>
+        </div>
+    	</article>        
+		</section>
+		</div> -->
+	</div>
+</body>
+</html>
